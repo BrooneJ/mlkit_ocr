658ab2c79f6d7b6ff4eca67b124a6c881e0a623b
HyungwonJin <hwjin92@gmail.com>
2025-08-03 16:26:35 +0900

    refactor: remove unused imports in CapturedScreen.kt

--- app/src/main/java/com/example/ocr/ui/CapturedScreen.kt
+++ app/src/main/java/com/example/ocr/ui/CapturedScreen.kt
@@ -3,13 +3,11 @@ package com.example.ocr.ui
 import android.content.res.Configuration
 import android.net.Uri
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
