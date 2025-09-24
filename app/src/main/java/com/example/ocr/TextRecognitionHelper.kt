b4a22b119a33fdd821bf72bcd1982994f8fdd117
BrooneJ <hwjin92@gmail.com>
2025-09-24 21:36:06 +0900

    feat: remove debug logging from text recognition process

--- app/src/main/java/com/example/ocr/TextRecognitionHelper.kt
+++ app/src/main/java/com/example/ocr/TextRecognitionHelper.kt
@@ -2,7 +2,6 @@ package com.example.ocr
 
 import android.content.Context
 import android.net.Uri
-import android.util.Log
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
@@ -17,19 +16,6 @@ object TextRecognitionHelper {
     val recognizer = TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build())
     recognizer.process(inputImage)
       .addOnSuccessListener { visionText ->
-        Log.d("TextRecognition", "Recognized text: ${visionText.text}")
-        for (block in visionText.textBlocks) {
-          val blockText = block.text
-          Log.d("TextRecognition", "Block text: $blockText")
-          for (line in block.lines) {
-            val lineText = line.text
-            Log.d("TextRecognition", "\tLine text: $lineText")
-            for (element in line.elements) {
-              val elementText = element.text
-              Log.d("TextRecognition", "\t\tElement text: $elementText")
-            }
-          }
-        }
         // Process the recognized text
         onResult(visionText.text)
       }
